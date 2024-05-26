33c6ad03f587f9c8e42590a0e1cdbb64ab1a8aec
Γιάννης Καρασούλας <ioanniskarasoulas@gmail.com>
2024-05-26 17:10:27 +0300

    Add playerBG blur value to global settings

--- lib/globalSettings.dart
+++ lib/globalSettings.dart
@@ -4,6 +4,8 @@ class GlobalSettings {
   static bool showNextSong = true;
   static bool stopPlayerOnDeviceDisconnect = true;
 
+  static double playerBGBlurValue = 2.0;
+
   void loadSettings() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
