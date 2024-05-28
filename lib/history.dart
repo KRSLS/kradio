0119d35bb0ab5545255b30f50821213355cdfac8
Γιάννης Καρασούλας <ioanniskarasoulas@gmail.com>
2024-05-28 12:25:16 +0300

    add ability to copy song and delete a single song

--- lib/history.dart
+++ lib/history.dart
@@ -1,6 +1,7 @@
 import 'package:KRadio/globalSettings.dart';
 import 'package:KRadio/historyData.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class History extends StatefulWidget {
   const History({super.key});
@@ -33,6 +34,47 @@ class _HistoryState extends State<History> {
           return ListTile(
             title: Text(HistoryData.history[index].songTitle),
             subtitle: Text('Station: ' + HistoryData.history[index].station),
+            onTap: () {
+              HapticFeedback.lightImpact();
+              Clipboard.setData(
+                  ClipboardData(text: HistoryData.history[index].songTitle));
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Song copied.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              );
+            },
+            onLongPress: () {
+              ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
+                  content: Text('Delete song from history?'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            HistoryData.history.removeAt(index);
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                'Song delete from history.',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          );
+                          ScaffoldMessenger.of(context).clearMaterialBanners();
+                        },
+                        child: Text('Yes')),
+                    TextButton(
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).clearMaterialBanners();
+                      },
+                      child: Text('No'),
+                    ),
+                  ]));
+            },
           );
         },
       ),
